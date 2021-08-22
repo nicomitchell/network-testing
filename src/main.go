33f61ea89eef9057b38f56eec4862b358d6894f7
Nicolas Mitchell <namitc02@louisville.edu>
2021-08-22 14:36:05 -0600

    update main

--- src/main.go
+++ src/main.go
@@ -1,23 +1,96 @@
 package main
 
 import (
-	"math/rand"
-	"time"
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
 
-	"github.com/go-grid-vis/src/display"
+	"github.com/nicomitchell/network-testing/src/node"
 )
 
 func main() {
-	grid := display.NewSquareDisplay(50)
-	go grid.Start()
-	//go func() {
-	defer grid.Stop()
-	for i := 0; i < 1000; i++ {
-		x := rand.Int() % 50
-		y := rand.Int() % 50
-		grid.UpdateCoord(x, y, (byte(rand.Int()%94) + ' '))
-		time.Sleep(17 * time.Millisecond)
+	if len(os.Args) != 4 {
+		fmt.Println("required args: ip list filepath, server port, gossip network ports")
+		os.Exit(0)
 	}
-	//}()
+	ipList := os.Args[1]
+	httpServerPort := os.Args[2]
+	portRange := os.Args[3]
+	f, err := os.Open(ipList)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	var lines []string
+	lines, err = getLines(f)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	// fmt.Println(httpServerPort)
+	openPorts, err := parsePortRange(portRange)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	// fmt.Println(openPorts)
+	// fmt.Println(lines)
+	node, err := node.NewNode(httpServerPort, openPorts)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	err = node.Start()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(lines)
 
+}
+
+func getLines(r io.ReadCloser) ([]string, error) {
+	br := bufio.NewReader(r)
+	l, err := br.ReadBytes('\n')
+	lines := make([]string, 0)
+	if err != nil {
+		if err != io.EOF {
+			return nil, err
+		}
+		lines = append(lines, strings.Trim(string(l), "\n"))
+		return lines, nil
+	}
+	for err == nil {
+		lines = append(lines, strings.Trim(string(l), "\n"))
+		l, err = br.ReadBytes('\n')
+	}
+	if err != io.EOF {
+		return nil, err
+	}
+	r.Close()
+	return lines, nil
+}
+
+func parsePortRange(portRange string) ([]string, error) {
+	startAndEnd := strings.Split(portRange, ":")
+	if len(startAndEnd) != 2 {
+		return nil, fmt.Errorf("Invalid formatting for port range")
+	}
+	start, err := strconv.Atoi(startAndEnd[0])
+	if err != nil {
+		return nil, err
+	}
+	end, err := strconv.Atoi(startAndEnd[1])
+	if err != nil {
+		return nil, err
+	}
+	out := []string{}
+	for i := start; i <= end; i++ {
+		port := strconv.Itoa(i)
+		out = append(out, port)
+	}
+	return out, nil
 }
